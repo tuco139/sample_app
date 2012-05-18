a0eff3467e329b1ec845154f41c5de4cf51633f0
John Cline <jac244@cornell.edu>
2012-05-18 08:40:38 -0400

    working on sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,10 +9,10 @@ describe "Users" do
       it "should not make a new user" do
         lambda do 
           visit signup_path
-          fill_in :user_email,         :with => ""
-          fill_in :user_email,        :with => ""
-          fill_in :user_password,     :with => ""
-          fill_in :user_password_confirmation, :with => ""
+          fill_in "Name",         :with => ""
+          fill_in "Email",        :with => ""
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
           click_button
           response.should render_template('users/new')
           response.should have_selector("div#error_explanation")
@@ -25,10 +25,10 @@ describe "Users" do
       it "should make a new user" do
         lambda do
           visit signup_path
-          fill_in :user_name,         :with => "Example User"
-          fill_in :user_email,        :with => "user@example.com"
-          fill_in :user_password,     :with => "foobar"
-          fill_in :user_password_confirmation, :with => "foobar"
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
           click_button
           response.should have_selector("div.flash.success", :content => "Welcome")  
           response.should render_template('users/show')
